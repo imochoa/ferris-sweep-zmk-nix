991a3c0fa27d98f02809502e19656edc480e9194
imochoa <18612100+imochoa@users.noreply.github.com>
2025-10-04 13:17:10 +0000

    added easier control+shift

--- config/34.h
+++ config/34.h
@@ -53,6 +53,8 @@
 #define RH1 33
 
 // Not in original file
-#define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB0 LB1 LB2 LB3 LB4  // Left-hand keys.
-#define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4  // Right-hand keys.
-#define THUMBS LH1 LH0 RH0 RH1                                              // Thumb keys.
+#define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB0 LB1 LB2 LB3 LB4 // Left-hand keys.
+#define KEYS_HRM_L LM1 LM2 LM3 LM4                                         // Left-hand HRM keys.
+#define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4 // Right-hand keys.
+#define KEYS_HRM_R RM1 RM2 RM3 RM4                                         // Right-hand HRM keys.
+#define THUMBS LH1 LH0 RH0 RH1                                             // Thumb keys.
